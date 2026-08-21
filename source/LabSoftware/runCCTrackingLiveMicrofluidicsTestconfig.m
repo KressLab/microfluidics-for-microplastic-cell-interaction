18a1c9b5e834c13352c39d94ed887d730d16e787
KressLab <matteo1.kumar@uni-bayreuth.de>
2026-08-21 12:26:19 +0200

    Update runCCTrackingLiveMicrofluidicsTestconfig.m
    Final Version

--- source/LabSoftware/runCCTrackingLiveMicrofluidicsTestconfig.m
+++ source/LabSoftware/runCCTrackingLiveMicrofluidicsTestconfig.m
@@ -1,11 +1,15 @@
-% ########################
-% Wolfgang Gross
-% University of Bayreuth
-% 26.03.2019
-% Run this on a windows machine without the actual hardware beeing
-% connected to test the main functionality of the code. The hardware is not
-% accessed.
-% ########################
+%Copyright 2026 Wolfgrang Gross, Matteo Kumar
+%
+%Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+%
+%Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+%Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+%Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+%
+%THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+%FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+%(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, 
+%STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  
 clear;
 close all;
